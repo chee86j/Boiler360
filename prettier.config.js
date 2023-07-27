0bdc818b98c535b1852c08326dfad72a74b36d8e
chee86j <jeffreychee86@gmail.com>
2023-07-27 11:12:58 -0400

    adjusted font family and daisyui

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: [],
+  prettierPath: "./node_modules/prettier",
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}", "./static/**/*.{html,css}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito Sans"', "sans-serif"],
+      },
+    },
   },
-  variants: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#22d3ee",
+
+          secondary: "#84cc16",
+
+          accent: "#ff7000",
+
+          neutral: "#D6D6D6",
+
+          "base-100": "#1f2937",
+
+          info: "#85d5e5",
+
+          success: "#62dfc8",
+
+          warning: "#c09f0c",
+
+          error: "#e6381a",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
-};
+}
